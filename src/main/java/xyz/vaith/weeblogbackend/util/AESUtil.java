ac48c84530d086b9ce3827b2b0f37e9411125536
Vaith <vaithwee@yeah.net>
2020-03-01 18:27:36 +0800

    Added ase uti

---
+++ src/main/java/xyz/vaith/weeblogbackend/util/AESUtil.java
@@ -0,0 +1,43 @@
+package xyz.vaith.weeblogbackend.util;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AESUtil {
+    private static final String KEY = "h2uh123haj89wjoj";
+    private static final String ALGORITHMSTR = "AES/ECB/PKCS5Padding";
+
+    public static String encrypt(String content, String encryptKey) throws Exception {
+        KeyGenerator kgen = KeyGenerator.getInstance("AES");
+        kgen.init(128);
+        Cipher cipher = Cipher.getInstance(ALGORITHMSTR);
+        cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(encryptKey.getBytes(), "AES"));
+        byte[] b = cipher.doFinal(content.getBytes("utf-8"));
+        return Base64.encodeBase64String(b);
+    }
+
+    public static String decrypt(String encryptStr, String decryptKey) throws Exception {
+        KeyGenerator kgen = KeyGenerator.getInstance("AES");
+        kgen.init(128);
+        Cipher cipher = Cipher.getInstance(ALGORITHMSTR);
+        cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(decryptKey.getBytes(), "AES"));
+        // 采用base64算法进行转码,避免出现中文乱码
+        byte[] encryptBytes = Base64.decodeBase64(encryptStr);
+        byte[] decryptBytes = cipher.doFinal(encryptBytes);
+        return new String(decryptBytes);
+    }
+
+    public static String encrypt(String content) throws Exception {
+        return encrypt(content, KEY);
+    }
+    public static String decrypt(String encryptStr) throws Exception {
+        return decrypt(encryptStr, KEY);
+    }
+
+}
