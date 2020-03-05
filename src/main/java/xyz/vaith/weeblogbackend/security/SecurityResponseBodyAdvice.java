bbd0c5167e2d81a566a0bb0e1caf512018772170
Vaith <vaithwee@yeah.net>
2020-03-05 17:35:49 +0800

    Added enable encrypt body function

--- src/main/java/xyz/vaith/weeblogbackend/security/SecurityResponseBodyAdvice.java
+++ src/main/java/xyz/vaith/weeblogbackend/security/SecurityResponseBodyAdvice.java
@@ -1,5 +1,6 @@
 package xyz.vaith.weeblogbackend.security;
 
+import com.alibaba.fastjson.JSON;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice(basePackages = "xyz.vaith.weeblogbackend.controller")
 public class SecurityResponseBodyAdvice implements ResponseBodyAdvice {
@@ -37,6 +40,17 @@ public class SecurityResponseBodyAdvice implements ResponseBodyAdvice {
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter methodParameter, MediaType mediaType, Class aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
         if (supportSecretResponse(methodParameter)) {
+
+            List<String> info = serverHttpRequest.getHeaders().get("info");
+            if (info != null && info.size() > 0) {
+                Map parse = (Map) JSON.parse(info.get(0));
+                int en = Integer.parseInt(parse.get("en").toString());
+                if (en == 0) {
+                    serverHttpResponse.getHeaders().set("en", "0");
+                    return body;
+                }
+            }
+
             ObjectMapper objectMapper = new ObjectMapper();
             try {
                 String result = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(body);
